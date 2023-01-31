913f8bc3ce397f3c5c7f4e350d3334030fca2158
dis-dotcom <71979445+dis-dotcom@users.noreply.github.com>
2023-01-31 22:34:39 +0300

    Update Car.cs
    Небольшие переименования

--- TxtToJson/Data/Models/Car.cs
+++ TxtToJson/Data/Models/Car.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace TxtToJson.Data.Models
@@ -15,4 +15,13 @@ namespace TxtToJson.Data.Models
 
         public int Price { get; set; }
     }
+    
+    //public class Car
+    //{
+    //    public int CarId { get; set; }            // ИД в файле нет, поэтому пока лучше убрать бы
+    //    public string CarName { get; set; }       // Это марка Brand 
+    //    public string CarPosition { get; set; }   // Это Model, + это и так понятно, что относится к Car
+    //    public int Year { get; set; }             
+    //    public int Price { get; set; }
+    //}
 }
